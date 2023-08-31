57b684ee605546859bde80c903661356fab4d775
khvatid <hv20027@gmail.com>
2023-08-31 06:33:44 +0300

    dev branch

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -47,17 +47,11 @@ dependencies {
 
 
     androidTestImplementation ("androidx.test.ext:junit-ktx:1.1.5")
-
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-
     androidTestImplementation ("com.google.truth:truth:1.1.2")
-
     androidTestImplementation ("androidx.arch.core:core-testing:2.2.0")
-
     androidTestImplementation ("androidx.test:rules:1.5.0")
-
     androidTestImplementation ("androidx.test:runner:1.5.2")
-
     androidTestImplementation ("androidx.test:core:1.5.0")
 
 }
